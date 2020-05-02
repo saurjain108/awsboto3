8c8f758740d1c84538b9865c272ac8656a3f22b1
Raunak <rony.jain108@gmail.com>
2020-05-02 03:40:41 +0000

    modified boto_script_using_clent.py

--- boto_script_using_clent.py
+++ boto_script_using_clent.py
@@ -10,17 +10,13 @@ ec2_con_cli=aws_man_con.client(service_name="ec2")
 #s3
 s3_con_cli=aws_man_con.client(service_name="s3")
 
-
 #listing all user using client object
 response=iam_con_cli.list_users()
 print(response)
 print("------------------------------------------")
 for item in response['Users']:
    # print(item)
-
     print(item['UserName'])
-
-
 
 #listing all ec2 instant id
 ec2_response=(ec2_con_cli.describe_instances())
@@ -32,5 +28,8 @@ for i in ec2_response['Reservations']:
         print(j['InstanceType'])
     print('----------------------------')
 
-
+#gettinf names of buckets in s3
+s3_response=(s3_con_cli.list_buckets())
+for x in s3_response['Buckets']:
+    print(x['Name'])
 
